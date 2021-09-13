91078e14b61526e0a9a5911351626cf9f76170ec
Eetu Raunio <eetu.raunio@helsinki.fi>
2021-09-14 02:47:08 +0300

    part1 exercise 1

---
+++ part1/src/App.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      {props.parts.map((part) => <p key={part.title}>{part.title} {part.exercises}</p>)}
+    </div>
+  )
+}
+
+const Total = (props) => {
+  const exercises = props.parts.map((part) => part.exercises).reduce((a, b) => a + b)
+
+  return (
+    <p>Number of exercises {exercises}</p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    { title: 'Fundamentals of React', exercises: 10 },
+    { title: 'Using props to pass data', exercises: 7 },
+    { title: 'State of a component', exercises: 14 }
+  ]
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  )
+}
+
+export default App;
