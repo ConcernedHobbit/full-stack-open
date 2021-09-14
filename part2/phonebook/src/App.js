9f3e0c11c50872c04fbaa82795e1c2861f55564c
Eetu Raunio <eetu.raunio@helsinki.fi>
2021-09-14 05:33:07 +0300

    part 2 exercise 9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -8,14 +8,19 @@ const Contact = ({contact}) => {
 
 const App = () => {
   const [contacts, setContacts] = useState([
-    { name: 'Arto Hellas', number: '0118-999-88199-9119-725-3' }
+    { name: 'Arto Hellas', number: '0118-999-88199-9119-725-3' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const handleNameChange = (event) => setNewName(event.target.value)
   const handleNumberChange = (event) => setNewNumber(event.target.value)
+  const handleFilterChange = (event) => setFilter(event.target.value)
 
   const addContact = (event) => {
     event.preventDefault()
@@ -39,6 +44,9 @@ const App = () => {
   return (
     <>
       <h2>Phonebook</h2>
+      <div>filter contacts by name: <input value={filter} onChange={handleFilterChange} /></div>
+
+      <h2>Add new contact</h2>
       <form onSubmit={addContact}>
         <div>name: <input value={newName} onChange={handleNameChange} /></div>
         <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
@@ -46,7 +54,10 @@ const App = () => {
       </form>
 
       <h2>Contacts</h2>
-      { contacts.map((contact) => <Contact key={contact.name} contact={contact} />) }
+      { 
+        contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()))
+                .map(contact => <Contact key={contact.name} contact={contact} />)
+      }
     </>
   )
 }
