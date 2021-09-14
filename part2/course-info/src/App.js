13cba6dcf38e949ddf5bed6868f05ad14152f5b5
Eetu Raunio <eetu.raunio@helsinki.fi>
2021-09-14 05:03:22 +0300

    part 2 exercise 5

--- part2/course-info/src/App.js
+++ part2/course-info/src/App.js
@@ -1,36 +1,5 @@
 import React from 'react'
-
-const Header = ({name}) => {
-  return (
-    <h2>{name}</h2>
-  )
-}
-
-const Part = ({part}) => {
-  return (
-    <p>{part.name} ({part.exercises} exercises)</p>
-  )
-}
-
-const Content = ({parts}) => {
-  const totalExercises = parts.map((part) => part.exercises).reduce((a, b) => a + b)
-
-  return (
-    <div>
-      {parts.map((part) => <Part key={part.id} part={part} />)}
-      <strong>Total exercises: {totalExercises}</strong>
-    </div>
-  )
-}
-
-const Course = ({course}) => {
-  return (
-    <>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-    </>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
