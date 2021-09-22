2f7259782eb43bbca9cb3b6c6583f4ef4c5c133c
Eetu Raunio <eetu.raunio@helsinki.fi>
2021-09-22 03:59:57 +0300

    part 3 exercise 7

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,8 @@
-const { response } = require('express')
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
+app.use(morgan('tiny'))
 app.use(express.json())
 
 let contacts = [
