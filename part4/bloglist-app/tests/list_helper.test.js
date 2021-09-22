160a2d95ebbbdbbf0e2d8b2abd973da9992ff3a7
Eetu Raunio <eetu.raunio@helsinki.fi>
2021-09-22 13:31:58 +0300

    part 4 exercise 5

--- part4/bloglist-app/tests/list_helper.test.js
+++ part4/bloglist-app/tests/list_helper.test.js
@@ -63,3 +63,68 @@ describe('total likes', () => {
         expect(result).toBe(40)
     })
 })
+
+describe('favorite blog', () => {
+    test('of empty list is null', () => {
+        const blogs = []
+        const result = listHelper.favoriteBlog(blogs)
+        expect(result).toBeNull()
+    })
+
+    test('with a list of one blog returns that blog', () => {
+        const blog = {
+            _id: '5a422b3a1b54a676234d17f9',
+            title: 'Another test blog',
+            author: 'Bored Tester',
+            url: 'http://inescure.website.com',
+            likes: 10,
+            __v: 0
+        }
+        const blogs = [blog]
+
+        const result = listHelper.favoriteBlog(blogs)
+        expect(result).toEqual(blog)
+    })
+
+    test('of a bigger list is calculated right', () => {
+        const blog = {
+            _id: '5a422b3a1b54a676234d17f9',
+            title: 'Another test blog',
+            author: 'Bored Tester',
+            url: 'http://inescure.website.com',
+            likes: 500,
+            __v: 0
+        }
+
+        const blogs = [
+            {
+                _id: '5a422b891b54a676234d17fa',
+                title: 'Robert Tech Quickies',
+                author: 'Robert Sebastian',
+                url: 'https://rtq.com',
+                likes: 5,
+                __v: 0
+            },
+            {
+                _id: '5a422aa71b54a632234e14f2',
+                title: 'Unique Blog',
+                author: 'Unqiue Person',
+                url: 'https://uniq.co',
+                likes: 320,
+                __v: 0
+            },
+            blog,
+            {
+                _id: '5a422aa71b54a676234d17f8',
+                title: 'Test Blog',
+                author: 'Testy McTestface',
+                url: 'https://example.com',
+                likes: 200,
+                __v: 0
+            }
+        ]
+
+        const result = listHelper.favoriteBlog(blogs)
+        expect(result).toEqual(blog)
+    })
+})
--- part4/bloglist-app/utils/list_helper.js
+++ part4/bloglist-app/utils/list_helper.js
@@ -6,7 +6,13 @@ const totalLikes = (blogs) => {
     return blogs.map(b => b.likes).reduce((a, b) => a + b, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) return null
+    return blogs.sort((a, b) => b.likes - a.likes)[0]
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
