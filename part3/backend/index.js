ebbd19e530651a8ec2ca12121b6105520c0ff191
Eetu Raunio <eetu.raunio@helsinki.fi>
2021-09-22 04:21:45 +0300

    part 3 prepare for heroku deployment

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -115,7 +115,7 @@ app.get('/info', (req, res) => {
     res.send(`<p>Phonebook has info for ${contacts.length} people</p><p>${new Date()}</p>`);
 })
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
