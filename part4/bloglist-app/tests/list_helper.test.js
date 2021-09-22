93e7e241f2727f79a0539654c4703b3af3beb7b2
Eetu Raunio <eetu.raunio@helsinki.fi>
2021-09-22 13:18:14 +0300

    part 4 exercise 4

--- part4/bloglist-app/tests/list_helper.test.js
+++ part4/bloglist-app/tests/list_helper.test.js
@@ -7,3 +7,59 @@ describe('dummy', () => {
         expect(result).toBe(1)
     })
 })
+
+describe('total likes', () => {
+    test('of empty list is zero', () => {
+        const blogs = []
+        const result = listHelper.totalLikes(blogs)
+        expect(result).toBe(0)
+    })
+
+    test('when list has only one blog equals the likes of that blog', () => {
+        const blogs = [
+            {
+                _id: '5a422aa71b54a676234d17f8',
+                title: 'Test Blog',
+                author: 'Testy McTestface',
+                url: 'https://example.com',
+                likes: 25,
+                __v: 0
+            }
+        ]
+
+        const result = listHelper.totalLikes(blogs)
+        expect(result).toBe(25)
+    })
+
+    test('of a bigger list is calculated right', () => {
+        const blogs = [
+            {
+                _id: '5a422aa71b54a676234d17f8',
+                title: 'Test Blog',
+                author: 'Testy McTestface',
+                url: 'https://example.com',
+                likes: 25,
+                __v: 0
+            },
+            {
+                _id: '5a422b3a1b54a676234d17f9',
+                title: 'Another test blog',
+                author: 'Bored Tester',
+                url: 'http://inescure.website.com',
+                likes: 10,
+                __v: 0
+            },
+            {
+                _id: '5a422b891b54a676234d17fa',
+                title: 'Robert Tech Quickies',
+                author: 'Robert Sebastian',
+                url: 'https://rtq.com',
+                likes: 5,
+                __v: 0
+            }
+        ]
+
+        const result = listHelper.totalLikes(blogs)
+        expect(result).toBe(40)
+    })
+})
