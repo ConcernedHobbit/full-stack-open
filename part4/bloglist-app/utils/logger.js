06072cd8efb1e671c66d9486148c2d90fd181c66
Eetu Raunio <eetu.raunio@helsinki.fi>
2021-12-02 20:22:52 +0200

    fix logging to respect environment

--- part4/bloglist-app/utils/logger.js
+++ part4/bloglist-app/utils/logger.js
@@ -1,8 +1,16 @@
 const info = (... params) => {
+    if (process.env.NODE_ENV === 'test') {
+        return
+    }
+
     console.info(... params)
 }
 
 const error = (...  params) => {
+    if (process.env.NODE_ENV === 'test') {
+        return
+    }
+
     console.error(... params)
 }
 
