c0f28ad79a24ddfe47c1b8c6e4357e4f9cb2e90d
Eetu Raunio <eetu.raunio@helsinki.fi>
2021-12-17 16:34:04 +0200

    feat: exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,31 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+
+const Blog = ({blog}) => {
+  const [visible, setVisible] = useState(false)
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  if (visible) {
+    return (
+      <div className='blog opened'>
+        <p onClick={toggleVisibility} className='clickable header'>
+          <span className='title'>{blog.title}</span> by <span className='author'>{blog.author}</span> 
+        </p>
+        <p>{blog.url}</p>
+        <p>{blog.likes} likes <button>like</button></p>
+        <p>{blog.user.name}</p>
+      </div>  
+    )
+  } else {
+    return (
+      <div className='blog closed'>
+        <p onClick={toggleVisibility} className='clickable header'>
+          <span className='title'>{blog.title}</span> by <span className='author'>{blog.author}</span> 
+        </p>
+      </div>
+    )
+  }
+}
 
 export default Blog
