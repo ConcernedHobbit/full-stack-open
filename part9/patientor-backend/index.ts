010de679ae2ea1180e1fd2ca9411c72696dbcced
Eetu Raunio <eetu.raunio@helsinki.fi>
2022-03-02 21:30:43 +0200

    9.10

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -1,10 +1,15 @@
 import express from "express";
+import { getDiagnoses } from "./services/diagnosisService";
 
 const app = express();
 app.use(express.json());
 
 app.get("/api/ping", (_req, res) => {
   res.send("pong");
+});
+
+app.get("/api/diagnoses", (_req, res) => {
+  res.send(getDiagnoses());
 });
 
 const PORT = 3001;
---
+++ part9/patientor-backend/services/diagnosisService.ts
@@ -0,0 +1,6 @@
+import diagnoses from "../data/diagnoses";
+import { Diagnose } from "../types";
+
+export function getDiagnoses(): Array<Diagnose> {
+  return diagnoses;
+}
