f8961a66764bf1787343b81c72ff70690b7a1b27
Eetu Raunio <eetu.raunio@helsinki.fi>
2022-09-15 00:50:07 +0300

    9.19

--- part9/patientor-backend/src/types.ts
+++ part9/patientor-backend/src/types.ts
@@ -10,7 +10,51 @@ export enum Gender {
   Other = 'other'
 }
 
-export type Entry = Record<string, never>;
+type BaseEntry = {
+  id: string;
+  specialist: string;
+  diagnosisCodes?: Diagnose['code'][];
+  description: string;
+  date: string;
+};
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+export type Discharge = {
+  date: string;
+  criteria: string;
+};
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
+}
+
+export type SickLeave = {
+  startDate: string;
+  endDate?: string;
+};
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export type Patient = {
   id: string;
