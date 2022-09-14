c24a48ee2c8b29e93a10e213f9711ea36095e4bd
Eetu Raunio <eetu.raunio@helsinki.fi>
2022-09-15 01:19:13 +0300

    9.20

--- part9/patientor/src/components/Patient.tsx
+++ part9/patientor/src/components/Patient.tsx
@@ -1,42 +1,68 @@
-import { Avatar, Card, CardContent, CardHeader, Typography } from "@material-ui/core";
+import {
+  Avatar,
+  Box,
+  Card,
+  CardContent,
+  CardHeader,
+  Chip,
+  Grid,
+  Typography,
+} from "@material-ui/core";
 import { Patient as PatientType } from "../types";
 import WorkIcon from "@material-ui/icons/Work";
 import CakeIcon from "@material-ui/icons/Cake";
 
 const Patient = ({ patient }: { patient: PatientType }) => {
   return (
-    <Card>
-      <CardHeader
-        avatar={
-          <Avatar>
-            {patient.name.charAt(0)}
-          </Avatar>
-        }
-        disableTypography
-        title={
-          <Typography variant="h5">
-            {patient.name}
-          </Typography>
-        }
-        subheader={
-          <Typography>
-            {patient.ssn}
-          </Typography>
-        }
-      />
+    <>
+      <Card>
+        <CardHeader
+          avatar={<Avatar>{patient.name.charAt(0)}</Avatar>}
+          disableTypography
+          title={<Typography variant="h5">{patient.name}</Typography>}
+          subheader={<Typography>{patient.ssn}</Typography>}
+        />
 
-      <CardContent>
-        <Typography style={{ display: "flex", alignItems: "center" }}>
-          <CakeIcon style={{ marginRight: ".5rem" }} aria-label="Birthday" />
-          {patient.dateOfBirth}
-        </Typography>
+        <CardContent>
+          <Typography style={{ display: "flex", alignItems: "center" }}>
+            <CakeIcon style={{ marginRight: ".5rem" }} aria-label="Birthday" />
+            {patient.dateOfBirth}
+          </Typography>
 
-        <Typography style={{ display: "flex", alignItems: "center" }}>
-          <WorkIcon style={{ marginRight: ".5rem" }} aria-label="Occupation" /> 
-          {patient.occupation}
-        </Typography>
-      </CardContent>
-    </Card>
+          <Typography style={{ display: "flex", alignItems: "center" }}>
+            <WorkIcon
+              style={{ marginRight: ".5rem" }}
+              aria-label="Occupation"
+            />
+            {patient.occupation}
+          </Typography>
+        </CardContent>
+      </Card>
+
+      {patient.entries && (
+        <Box sx={{ marginTop: "1rem" }}>
+          <Grid container spacing={2}>
+            {patient.entries.map((entry) => (
+              <Grid item key={entry.id} xs>
+                <Card>
+                  <CardHeader
+                    title={entry.date}
+                    subheader={<Typography>{entry.specialist}</Typography>}
+                  />
+                  <CardContent>
+                    <Typography>{entry.description}</Typography>
+                    {entry.diagnosisCodes &&
+                      entry.diagnosisCodes.map((code) => (
+                        <Chip key={code} label={code} variant="outlined" />
+                      ))}
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
+      )}
+    </>
   );
 };
 
--- part9/patientor/src/types.ts
+++ part9/patientor/src/types.ts
@@ -10,6 +10,52 @@ export enum Gender {
   Other = "other"
 }
 
+type BaseEntry = {
+  id: string;
+  specialist: string;
+  diagnosisCodes?: Diagnosis['code'][];
+  description: string;
+  date: string;
+};
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+export type Discharge = {
+  date: string;
+  criteria: string;
+};
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
+}
+
+export type SickLeave = {
+  startDate: string;
+  endDate?: string;
+};
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
 export interface Patient {
   id: string;
   name: string;
@@ -17,4 +63,5 @@ export interface Patient {
   gender: Gender;
   ssn?: string;
   dateOfBirth?: string;
+  entries: Entry[];
 }
